0e14188149ba595e161d278ce287fdd64d96d369
roriwa <103991412+roriwa@users.noreply.github.com>
2024-10-30 12:41:20 +0100

    added another cli argument to specify the algorithm used

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,29 @@
 #include "helper.h"
 #include "core.h"
 
+std::map<std::string, cv::Mat(*)(cv::Mat)> converter_functions = {
+    {"hsv", &convert_to_hsv},
+    {"gray", &convert_to_grayscale},
+    {"grayscale", &convert_to_grayscale},
+    {"emboss", &convert_to_emboss},
+};
+
+
 int main(const int argc, char** argv) {
-    if (argc != 2) {
-        std::cout << "Usage: " << argv[0] << " <image>" << std::endl;
+    if (argc != 3) {
+        const std::vector<std::string> supported_converters = extract_keys(converter_functions);
+        std::cout << "Usage: " << argv[0] << " {" << str_join(supported_converters, ",") << "} <image>" << std::endl;
         return EXIT_FAILURE;
     }
-    const std::string input_file = argv[1];
+
+    const std::string converter_name = argv[1];
+    const auto converter = converter_functions.find(converter_name);
+    if (converter == converter_functions.end()) {
+        std::cout << "Unknown conversion function '" << converter_name << "'" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    const std::string input_file = argv[2];
     if (!file_exists(input_file)) {
         std::cout << "File '" << input_file << "' does not exist" << std::endl;
         return EXIT_FAILURE;
@@ -23,10 +40,10 @@ int main(const int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
-    const cv::Mat output_image = convert_to_emboss(raw_image);
+    const cv::Mat output_image = converter->second(raw_image);
 
-    const std::string hsv_output_file = add_filename_suffix(input_file, "-out");
-    const bool hsv_success = cv::imwrite(hsv_output_file, output_image);
+    const std::string output_file = add_filename_suffix(input_file, "-out");
+    const bool hsv_success = cv::imwrite(output_file, output_image);
     if (!hsv_success) {
         std::cout << "Failed to save output image" << std::endl;
         return EXIT_FAILURE;
