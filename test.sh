f1485f4cf4853fb193fee59bc2ebd92ec1613edd
roriwa <103991412+roriwa@users.noreply.github.com>
2025-01-31 17:57:35 +0100

    updated test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,16 @@
 set -e
 shopt -s nullglob
 
-N=10
-ALGO="mpi-grayscale"
+function join_by {
+  local IFS="$1"
+  shift
+  echo "$*"
+}
+
+NL=$'\n'
+
+METHODS=("plain" "omp" "mpi" "mpi-omp" "cv2")
+ALGORITHMS=("grayscale" "hsv" "emboss")
 
 THIS="$(realpath "$(dirname "${BASH_SOURCES[0]}")")"
 BUILD="$THIS/build"
@@ -12,19 +20,41 @@ PROG="$BUILD/progko"
 
 mkdir -p "$BUILD" && cd "$BUILD"
 cmake ..
-make
+make -j
 
 cd "$TEST_IMAGES"
 
-for img in */*-out*; do
-  echo "rm '$img'"
-  rm "$img"
-done
-
-true > log.txt
-
-for img in */*; do
-  echo "-------------------------" | tee -a log.txt
-  echo "Testing: '$img'" | tee -a log.txt
-  NO_SHOW=1 multitime -q -s 1 -n $N "$PROG" "$ALGO" "$img" 2>&1 | tee -a log.txt
+for ALGO in "${ALGORITHMS[@]}"; do
+  echo "Testing '$ALGO'"
+#  for IMGDIR in */; do
+  for IMGDIR in "dice/" "giant-images"; do
+    echo "In Directory '$IMGDIR'"
+    for img in "${IMGDIR%/}"/*-out*; do
+      rm "$img"
+    done
+    OUTPUT="$ALGO,$(join_by ',' "${METHODS[@]}")$NL"
+    for IMG in "${IMGDIR%/}"/*; do
+      echo "On File '$IMG'"
+      OUTPUT+="$(basename "$IMG")"
+      for METHOD in "${METHODS[@]}"; do
+        echo "With Method '$METHOD'"
+        CMD=("$PROG")
+        if [[ "$METHOD" =~ ^mpi ]]; then
+          CMD=("mpirun" "$PROG")
+        fi
+        TIME="$(for _ in {1..5}; do
+          NO_SHOW=1 "${CMD[@]}" "$METHOD-$ALGO" "$IMG"
+          echo
+          sleep 0.5
+        done | sort -n | head -n 1)"
+        OUTPUT+=",$TIME"
+        sleep 1
+      done
+      OUTPUT+="$NL"
+      sleep 1
+    done
+    echo "Writing CSV '$ALGO-${IMGDIR%/}.csv'"
+    echo -n "$OUTPUT" > "$ALGO-${IMGDIR%/}.csv"
+#    exit 0
+  done
 done
